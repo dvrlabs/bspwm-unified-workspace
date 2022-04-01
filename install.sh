313d6661ae0d85c20ea9877c35daf335c27f57f7
dvr <dvrlabs.tv@gmail.com>
2022-04-01 07:42:04 -0400

    Fri Apr  1 07:42:04 AM EDT 2022

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-echo "Installing configs..."
+echo "Installing programs..."
+sudo apt install bspwm sxhkd polybar picom nitrogen arandr autorandr xscreensaver
 
+echo "Making directories..."
 mkdir -p $HOME/.config/bspwm
 mkdir -p $HOME/.config/sxhkd
 mkdir -p $HOME/.config/polybar
 
+echo "Copying configs..."
 cp bspwmrc $HOME/.config/bspwm/bspwmrc
 cp sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 cp config.ini $HOME/.config/polybar/config.ini
 cp launch.sh $HOME/.config/polybar/launch.sh
 cp changeAudio.sh $HOME/.config/polybar/changeAudio.sh
 
-sudo apt install bspwm sxhkd polybar picom nitrogen arandr autorandr xscreensaver
+
+echo "Installing fonts..."
+cp -r SauceCodePro /usr/share/fonts/truetype/
+cp -r noto /usr/share/fonts/truetype/
+fc-cache -f -v
+
