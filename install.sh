5e0dedb63852eecf2f56f2ceeb5ffa402882d1e2
dvr <dvrlabs.tv@gmail.com>
2022-04-19 14:09:11 -0400

    Tue Apr 19 02:09:11 PM EDT 2022

--- install.sh
+++ install.sh
@@ -13,11 +13,14 @@ cp bspwmrc $HOME/.config/bspwm/bspwmrc
 cp sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 cp config.ini $HOME/.config/polybar/config.ini
 cp launch.sh $HOME/.config/polybar/launch.sh
-cp changeAudio.sh $HOME/.config/polybar/changeAudio.sh
-
+cp changeAudio.sh $HOME/.config/polybar/changeAudio.sh 
+cp headphoneAudio.sh $HOME/.config/polybar/headphoneAudio.sh 
+cp speakerAudio.sh $HOME/.config/polybar/speakerAudio.sh
 
 echo "Installing fonts..."
-cp -r SauceCodePro /usr/share/fonts/truetype/
-cp -r noto /usr/share/fonts/truetype/
+cp -r fonts/SauceCodePro /usr/share/fonts/truetype/
+cp -r fonts/noto /usr/share/fonts/truetype/
+cp -r fonts/hack /usr/share/fonts/truetype/
+
 fc-cache -f -v
 
